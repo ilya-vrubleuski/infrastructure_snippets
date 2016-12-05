222ce3087f235690fe7ccda139e78a077846750d
vrublevsky_ilya <vrublevsky.ilya@gmail.com>
2016-12-05 23:51:10 +0300

    Create db path directory if not exists

--- mongo_runner/mongo_runner.ps1
+++ mongo_runner/mongo_runner.ps1
@@ -1,6 +1,8 @@
 ﻿$data = ( Get-Content .\config.json|ConvertFrom-Json);
 
-$filepath = Join-Path $data.mongoroot mongod.exe
-$dbpath=$data.dbstorage
+$filepath = Join-Path $data.mongoroot mongod.exe;
+$dbpath=$data.dbstorage;
+
+New-Item -ItemType Directory -Force -Path $dbpath;
 
 & $filepath --dbpath=$dbpath;
